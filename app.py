e33948b9115fadfbf3cef0a287e736870b6b9718
Nautik <nautik1@n-p.fr>
2021-11-28 17:01:38 +0100

    Add working draw and reset possibility ⏮️

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import helpers
 
 app = Flask(__name__)
@@ -8,8 +8,9 @@ app = Flask(__name__)
 def home(path):
     return "You need to go to a draw URL."
 
+
 @app.route("/<draw_name>/draw")
-def draw(draw_name):
+def show_draw(draw_name):
     participants = helpers.get_drawing_participants(draw_name)
 
     if not participants:
@@ -17,6 +18,44 @@ def draw(draw_name):
 
     return render_template("draw.html", draw_name=draw_name, participants=participants)
 
+
+@app.route("/<draw_name>/draw/<participant_name>", methods=["POST"])
+def participate(draw_name, participant_name):
+    remaining_participants = helpers.get_drawing_participants(draw_name)
+
+    if not remaining_participants:
+        return "Tirage non trouve"
+
+    participant = [
+        remaining_participants.pop(idx)
+        for idx, value in enumerate(remaining_participants)
+        if value.get("name") == participant_name
+    ]
+
+    if len(participant) != 1:
+        return "Participant non trouve"
+
+    offers_to = helpers.draw(remaining_participants)
+
+    participant = participant[0]
+    helpers.store_participation(draw_name, participant, offers_to)
+
+    return render_template(
+        "drawn.html",
+        draw_name=draw_name,
+        participant=participant,
+        offers_to=offers_to,
+    )
+
+
+@app.route("/<draw_name>/draw", methods=["DELETE"])
+def reset(draw_name):
+    user = request.headers.get('user', None)
+    if not user or not helpers.can_reset(draw_name, user):
+        return "You cannot reset this draw."
+
+    helpers.reset_draw(draw_name)
+    return "Reset done."
 
 if __name__ == "__main__":
     app.run()
--- helpers.py
+++ helpers.py
@@ -1,11 +1,61 @@
 import yaml
+import random
 
 DRAWINGS_FILEPATH = "drawings.yaml"
 
 
 def get_drawing_participants(draw_name):
+    with open(DRAWINGS_FILEPATH) as f:
+        drawings = yaml.safe_load(f)
+    return drawings.get(draw_name)
 
+
+def draw(participants):
+    not_drawn = [p for p in participants if not p.get("drawn")]
+    not_already_played = [p for p in not_drawn if not p.get("offers_to")]
+
+    # Special case: if only 2 people can be picked but one of them have not
+    # played yet, we need to pick him otherwise he won't have anyone to pick
+    if len(not_drawn) == 2 and len(not_already_played) == 1:
+        return not_already_played[0]
+
+    return random.choice(not_drawn)
+
+
+def store_participation(draw_name, participant, offers_to):
     with open(DRAWINGS_FILEPATH) as f:
         drawings = yaml.safe_load(f)
 
-    return drawings.get(draw_name)
+    for person in drawings[draw_name]:
+        if person.get("name") == participant.get("name"):
+            person["offers_to"] = offers_to.get("name")
+        elif person.get("name") == offers_to.get("name"):
+            person["drawn"] = True
+
+    with open(DRAWINGS_FILEPATH, "w") as f:
+        yaml.dump(drawings, f)
+
+
+def reset_draw(draw_name):
+    with open(DRAWINGS_FILEPATH) as f:
+        drawings = yaml.safe_load(f)
+
+    for person in drawings[draw_name]:
+        person.pop("offers_to", None)
+        person.pop("drawn", None)
+
+    with open(DRAWINGS_FILEPATH, "w") as f:
+        yaml.dump(drawings, f)
+
+
+def can_reset(draw_name, user_name):
+    with open(DRAWINGS_FILEPATH) as f:
+        drawings = yaml.safe_load(f)
+
+    can_reset = [
+        p
+        for p in drawings[draw_name]
+        if p.get("can_reset", None) and p["name"] == user_name
+    ]
+
+    return len(can_reset) > 0
