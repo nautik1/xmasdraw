0f76582d27b69551550d610730075825b9d282f7
Nautik <github@nautik.pm>
2023-12-06 21:26:59 +0100

    Fix bug: we don't care about the last participant if he is not in the choices at hand 🐛

--- xmasdraw/tests/test_helpers.py
+++ xmasdraw/tests/test_helpers.py
@@ -1,3 +1,5 @@
+import pytest
+
 from ..helpers import draw
 from .names import BORING_NAMES
 
@@ -25,3 +27,22 @@ def test_last_participant_needs_to_have_someone_to_pick():
     for i in range(99999):  # beat the odds
         drawn = draw(participants, "toto")
         assert drawn == "tata"
+
+
+@pytest.mark.parametrize("nb_of_participants", range(3, len(BORING_NAMES)))
+def test_dumb_draw_until_done(nb_of_participants):
+    participants = [{"name": name} for name in BORING_NAMES[:nb_of_participants]]
+
+    already_drawn = []
+    for player in participants:
+        player_name = player["name"]
+        drawn = draw(participants, player_name)
+
+        assert drawn not in already_drawn
+        assert drawn != player_name
+
+        # Take draw into account for next round
+        already_drawn.append(drawn)
+        for p in participants:
+            if p["name"] == player_name:
+                p["offers_to"] = drawn
